54ab413e1a1fbf0084ab9a37d1d4b8f38fa1705b
callmechristian <siman.christian@gmail.com>
2023-10-26 17:57:17 -0400

    usage comments

--- utils/SystemMonitor.py
+++ utils/SystemMonitor.py
@@ -13,6 +13,53 @@ class SystemMonitor:
         self.interval = 1
 
     def start_monitor(self, interval=1):
+        """
+        Start monitoring system resources at regular intervals.
+
+        Args:
+            interval (int, optional): The time interval in seconds at which system resource snapshots will be collected.
+            Defaults to 1 second.
+
+        Usage:
+            1. Import the SystemMonitor class from your script where it's defined.
+
+                from your_script import SystemMonitor
+
+            2. Create an instance of the SystemMonitor class.
+
+                monitor = SystemMonitor()
+
+            3. Start the monitoring with the desired interval using the start_monitor method. Optionally, you can specify
+            the monitoring interval in seconds as an argument. If not provided, the default interval of 1 second will
+            be used.
+
+                # Start monitoring with the default interval (1 second)
+                monitor.start_monitor()
+
+                # Start monitoring with a custom interval, e.g., 5 seconds
+                monitor.start_monitor(interval=5)
+
+            4. Once monitoring has started, the system resource snapshots will be collected at the specified interval.
+
+            5. To stop the monitoring, you can use the stop_monitor method.
+
+                monitor.stop_monitor()
+
+        Example:
+            from your_script import SystemMonitor
+
+            # Create an instance of SystemMonitor
+            monitor = SystemMonitor()
+
+            # Start monitoring with a custom interval of 3 seconds
+            monitor.start_monitor(interval=3)
+
+            # Allow monitoring for a specific duration
+            time.sleep(10) # can also just be another function call, not necessarily a sleep
+
+            # Stop monitoring
+            monitor.stop_monitor()
+        """
         if self.monitoring:
             print("Monitoring is already running.")
             return
@@ -81,6 +128,34 @@ class SystemMonitor:
 
         print("Average Snapshot:")
         print(avg_snapshot)
+    
+    def compute_and_return_average(self):
+        '''
+        Return the average system resources snapshot. <dict>
+
+        snapshot = {
+            "timestamp": time,
+            "cpu_percent": cpu_percent,
+            "memory_percent": memory_info.percent,
+            "read_count": read_count_diff,
+            "write_count": write_count_diff,
+            "read_bytes": read_bytes_diff,
+            "write_bytes": write_bytes_diff,
+        }
+        '''
+        if not self.snapshots:
+            print("No snapshots available for computing the average.")
+            return
+
+        avg_snapshot = {}
+        num_snapshots = len(self.snapshots)
+
+        for key in self.snapshots[0]:
+            if key == "timestamp":
+                continue
+            avg_snapshot[key] = sum(snapshot[key] for snapshot in self.snapshots) / num_snapshots
+
+        return avg_snapshot
     
     def write_snapshots_to_csv(self, filename, extensive=False):
         snapshots_to_write = self.snapshots if extensive else [self.compute_average_snapshot()]
